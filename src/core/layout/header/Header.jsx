fcde3632bcea7f04f4621ab93127df0d5ed90c31
Nicoleta Esanu <nicoleta_esanu@yahoo.fr>
2025-03-25 20:28:33 +0100

    Refactor header component: update job title and clean up CV download link

--- src/core/layout/header/Header.jsx
+++ src/core/layout/header/Header.jsx
@@ -3,6 +3,7 @@ import styles from "./Header.module.scss";
 import portraitImg from "../../../assets/images/portrait_Nicoleta.webp";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faDownload } from "@fortawesome/free-solid-svg-icons";
+// import {download_link} from "./Header.module.scss";
 
 const Header = () => {
   const navigate = useNavigate();
@@ -10,69 +11,77 @@ const Header = () => {
   const handleHomeClick = () => {
     navigate("/");
   };
-return (
+  return (
     <header className={styles.header}>
-        <div className={styles.nav_header} onClick={handleHomeClick}>
-            <img
-                src={portraitImg}
-                alt="Nicoleta profil"
-                className={styles.nav_header_img}
-            />
-            <h1 className={styles.title}>Développeuse Web Junior</h1>
-        </div>
-        <nav className={styles.nav}>
-            <ul>
-                <li>
-                    <NavLink
-                        to="/"
-                        className={({ isActive }) =>
-                            isActive ? styles.active : undefined
-                        }
-                    >
-                        Accueil
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink
-                        to="/about"
-                        className={({ isActive }) =>
-                            isActive ? styles.active : undefined
-                        }
-                    >
-                       À propos
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink
-                        to="/contact"
-                        className={({ isActive }) =>
-                            isActive ? styles.active : undefined
-                        }
-                    >
-                        Contact
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink
-                        to="/projets"
-                        className={({ isActive }) =>
-                            isActive ? styles.active : undefined
-                        }
-                    >
-                        Projets
-                    </NavLink>
-                </li>
-            </ul>
-            <a
+      <div className={styles.nav_header} onClick={handleHomeClick}>
+        <img
+          src={portraitImg}
+          alt="Nicoleta profil"
+          className={styles.nav_header_img}
+        />
+        <h1 className={styles.title}>Développeuse Web</h1>
+      </div>
+      <nav className={styles.nav}>
+        <ul>
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive ? styles.active : undefined
+              }
+            >
+              Accueil
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/about"
+              className={({ isActive }) =>
+                isActive ? styles.active : undefined
+              }
+            >
+              À propos
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/contact"
+              className={({ isActive }) =>
+                isActive ? styles.active : undefined
+              }
+            >
+              Contact
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/projets"
+              className={({ isActive }) =>
+                isActive ? styles.active : undefined
+              }
+            >
+              Projets
+            </NavLink>
+          </li>
+        </ul>
+        <a
                 href={import.meta.env.VITE_PUBLIC_URL + "/assets/document/cv.pdf"}
                 download="cv.pdf"
                 className={styles.download_link}
             >
               CV  <FontAwesomeIcon icon={faDownload} size="1x" />
             </a>
-        </nav>
+
+        {/* <a
+          href="/assets/document/cv.pdf"
+          download="cv.pdf"
+          className={styles.download_link}
+        >
+          CV <FontAwesomeIcon icon={faDownload} size="1x" />
+        </a> */}
+      </nav>
     </header>
-);
+  );
 };
 
 export default Header;
